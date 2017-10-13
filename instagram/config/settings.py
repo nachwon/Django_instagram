1e48d0b6e4c869cedff33acdb4f3fa5abefe6a69
Che1 <nachwon@gmail.com>
2017-10-13 16:50:01 +0900

    MEDIA_URL returns files in the MEDIA_ROOT folder

--- instagram/config/settings.py
+++ instagram/config/settings.py
@@ -17,6 +17,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 ROOT_DIR = os.path.dirname(BASE_DIR)
 CONFIG_SECRET_DIR = os.path.join(ROOT_DIR, '.config_secret')
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
--- instagram/post/models.py
+++ instagram/post/models.py
@@ -2,18 +2,17 @@ from django.db import models
 
 
 class Post(models.Model):
-    name = models.CharField(max_length=50)
-    photo = models.ImageField(upload_to='photo')
+    photo = models.ImageField(upload_to='post')
     created_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name
+        return f'Post {self.pk}'
 
 
-class PostComment:
+class PostComment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     content = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.content
+        return f'Comment {self.pk}'
