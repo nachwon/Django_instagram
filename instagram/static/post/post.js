d1a6f1c010c5c841f5922ff38299b43739a4bdc2
Che1 <nachwon@gmail.com>
2017-10-18 22:26:18 +0900

    minor css fixed

--- instagram/static/post/post.js
+++ instagram/static/post/post.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {myFunction()};
 
 function myFunction() {
-    if (document.body.scrollTop > 77 || document.documentElement.scrollTop > 77) {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
         document.getElementById("insta-head").className = "header header-scrolled";
         document.getElementById("insta-head-wrapper").className = "header-wrapper wrapper-scrolled";
         document.getElementById("insta-logo").style.height="0px";
